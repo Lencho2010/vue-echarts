fa69c445ee069d84d4f911ce4ee9ba48cd9ae193
Lencho2010 <335999737@qq.com>
2021-08-04 17:07:52 +0800

    添加登录页，路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import FlexApp from "../views/FlexApp";
+import TheLogin from "../views/TheLogin";
 
 Vue.use(VueRouter);
 
@@ -8,6 +9,11 @@ const routes = [
   {
     path: "/",
     redirect: "/flex"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: TheLogin
   },
   {
     path: "/flex",
@@ -27,6 +33,21 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+//全局前置路由守卫————初始化的时候被调用、每次路由切换之前被调用
+router.beforeEach((to, from, next) => {
+  console.log("前置路由守卫", to, from);
+  if (sessionStorage.getItem("user") || to.name === "login") {
+    next();
+  } else {
+    router.push("/login");
+  }
+});
+
+//全局后置路由守卫————初始化的时候被调用、每次路由切换之后被调用
+router.afterEach((to, from) => {
+
 });
 
 export default router;
